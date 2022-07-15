53da6e0050c6cbf936e2a35bba1e88796242f093
ask-khan <ahmedsaboorkhannu@gmail.com>
2022-07-15 14:35:25 +0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs'
+import { from, of } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -9,22 +10,32 @@ import { from, of } from 'rxjs'
 export class AppComponent implements OnInit {
   name = 'Angular';
   ngOnInit () {
-    of(2,3,4,5,6).subscribe( item => {
-      console.log( item );
-    })
+    // of(2,3,4,5,6).subscribe( item => {
+    //   console.log( item );
+    // })
 
-    from([2,3,4,5,6]).subscribe({
+    from([2,3,4,5,6]).pipe(
+      tap(val =>  { console.log( 'value', val ); }),
+      map( item => item * 2 ),
+      map( item => {
+        if ( item == 0 ) {
+          throw new Error("Zero")
+        }
+        return item;
+      }),
+      take(3)
+    ).subscribe({
       next:(item) => console.log( "item result", item ),
       error:(err) => console.log( "item error", err ),
       complete:() => console.log( 'complete' )
 
     })
 
-    of(2,3,4,5,6).subscribe({
-      next:(item) => console.log( "item result", item ),
-      error:(err) => console.log( "item error", err ),
-      complete:() => console.log( 'complete' )
+    // of(2,3,4,5,6).subscribe({
+    //   next:(item) => console.log( "item result", item ),
+    //   error:(err) => console.log( "item error", err ),
+    //   complete:() => console.log( 'complete' )
 
-    })
+    // })
   }  
 }
